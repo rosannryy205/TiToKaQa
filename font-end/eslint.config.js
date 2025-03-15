ea30e17e20eaa7cfe86f3df0331c4c61590ed41c
rosannryy205 <khanhvmps39093@gmail.com>
2025-03-15 12:14:05 +0700

    up

---
+++ font-end/eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import oxlint from 'eslint-plugin-oxlint'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  ...oxlint.configs['flat/recommended'],
+]
