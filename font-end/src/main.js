becc3767e9a29c984674c800a088f3b6d22a4b6e
rosannryy205 <khanhvmps39093@gmail.com>
2025-03-15 12:41:43 +0700

    update-bt

--- font-end/src/main.js
+++ font-end/src/main.js
@@ -1,11 +1,33 @@
-import './assets/main.css'
+// import './assets/main.css'
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+
+// const app = createApp(App)
+
+// app.use(router)
+
+// app.mount('#app')
+
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+// Thêm dòng này để import Font Awesome 
+import "@fortawesome/fontawesome-free/css/all.min.css"
+
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
