c6dc21f34a404f3b22f9b7418928b501f89085d6
rosannryy205 <khanhvmps39093@gmail.com>
2025-03-15 12:52:48 +0700

    fix

--- font-end/src/main.js
+++ font-end/src/main.js
@@ -1,4 +1,4 @@
-// import './assets/main.css'
+
 
 // import { createApp } from 'vue'
 // import App from './App.vue'
@@ -12,7 +12,6 @@
 
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
@@ -20,13 +19,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import './assets/main.css'
+
 
 // Thêm dòng này để import Font Awesome 
 import "@fortawesome/fontawesome-free/css/all.min.css"
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
