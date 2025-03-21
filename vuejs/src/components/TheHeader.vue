0b47b1e89033443c23372f3a78ae5c6911037659
rosannryy205 <khanhvmps39093@gmail.com>
2025-03-21 19:06:48 +0700

    update

--- vuejs/src/components/TheHeader.vue
+++ vuejs/src/components/TheHeader.vue
@@ -1,74 +1,79 @@
 <template>
- <!-- top header-->
-<div class="header">
+  <!-- top header-->
+  <div class="header">
     <div class="container">
-        <div class="navbar-top">
-            <nav class="navbar navbar-expand-lg navbar-bottom">
-                <div class="container">
-                    <!-- menu small screen  -->
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-            </nav>
-            <div class="nav-icons d-none d-lg-block">
-                <button class="icon-btn me-3"><i class="bi bi-people"></i></button>
-                <button class="icon-btn"><i class="bi bi-search"></i></button>
-            </div>
+      <div class="navbar-top">
+        <nav class="navbar navbar-expand-lg navbar-bottom">
+          <div class="container">
+            <!-- menu small screen  -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        </nav>
+        <div class="nav-icons d-none d-lg-block">
+          <button class="icon-btn me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
+            <i class="bi bi-people"></i>
+          </button>
 
-            <div class="logo-container">
-                <img src="../../public/img/logonew.png" alt="Logo" class="logo">
-            </div>
-
-            <div class="nav-icons d-none d-lg-block">
-                <button class="icon-btn me-3"><i class="bi bi-telephone"></i></button>
-                <button class="icon-btn"><i class="bi bi-cart"></i></button>
-            </div>
+          <button class="icon-btn"><i class="bi bi-search"></i></button>
         </div>
 
-        <!-- menu bottom -->
-        <nav class="navbar navbar-expand-lg navbar-bottom">
-            <div class="collapse navbar-collapse justify-content-center d-none d-lg-flex">
-                <ul class="navbar-nav fs-5">
-                    <li class="nav-item"><a class="nav-link" href="#">Trang chủ</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Mì cay</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Mì trộn</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Lẩu</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Món ăn khác</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Thức uống</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Topping</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Đặt bàn</a></li>
-                </ul>
-            </div>
-        </nav>
+        <div class="logo-container">
+          <img src="../../public/img/logonew.png" alt="Logo" class="logo">
+        </div>
+
+        <div class="nav-icons d-none d-lg-block">
+          <button class="icon-btn me-3"><i class="bi bi-telephone"></i></button>
+          <router-link to="/cart" style="color: black;">
+            <button class="icon-btn"><i class="bi bi-cart"></i></button>
+          </router-link>
+        </div>
+      </div>
+
+      <!-- menu bottom -->
+      <nav class="navbar navbar-expand-lg navbar-bottom">
+        <div class="collapse navbar-collapse justify-content-center d-none d-lg-flex">
+          <ul class="navbar-nav fs-5">
+            <li class="nav-item"><a class="nav-link" href="#">Trang chủ</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Mì cay</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Mì trộn</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Lẩu</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Món ăn khác</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Thức uống</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Topping</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Đặt bàn</a></li>
+          </ul>
+        </div>
+      </nav>
     </div>
 
     <!-- offcanvas menu small screen -->
     <div class="offcanvas offcanvas-start" id="offcanvasMenu">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title">Menu</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
-        </div>
-        <div class="offcanvas-body">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="#">Trang chủ</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Mì cay</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Mì trộn</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Lẩu</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Món ăn khác</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Thức uống</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Topping</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Đặt bàn</a></li>
-                <div class="icon">
-                    <button class="icon-btn me-3"><i class="bi bi-people"></i></button>
-                    <button class="icon-btn"><i class="bi bi-search"></i></button>
-                    <button class="icon-btn me-3"><i class="bi bi-telephone"></i></button>
-                    <button class="icon-btn"><i class="bi bi-cart"></i></button>
-                </div>
-            </ul>
-        </div>
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title">Menu</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="#">Trang chủ</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Mì cay</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Mì trộn</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Lẩu</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Món ăn khác</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Thức uống</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Topping</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Đặt bàn</a></li>
+          <div class="icon">
+            <button class="icon-btn me-3"><i class="bi bi-people"></i></button>
+            <button class="icon-btn"><i class="bi bi-search"></i></button>
+            <button class="icon-btn me-3"><i class="bi bi-telephone"></i></button>
+            <button class="icon-btn"><i class="bi bi-cart"></i></button>
+          </div>
+        </ul>
+      </div>
     </div>
-</div>
+  </div>
 
   <!-- Modal đăng nhập -->
   <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
