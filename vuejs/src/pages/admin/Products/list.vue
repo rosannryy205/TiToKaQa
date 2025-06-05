07cafb90f502e61ec61cc26b5424ce5fd44e3b38
thuytien <tiennttps39163@gmail.com>
2025-06-05 16:37:18 +0700

    list product

--- vuejs/src/pages/admin/Products/list.vue
+++ vuejs/src/pages/admin/Products/list.vue
@@ -2,8 +2,8 @@
   <h3 class="title">Quản lý danh mục</h3>
 
   <div class="mb-4 d-flex align-items-center gap-3 flex-wrap">
-    <router-link :to="{ name: 'insert-category' }" class="btn btn-add">
-      + Thêm danh mục
+    <router-link :to="{ name: 'insert-food' }" class="btn btn-add">
+      + Thêm món ăn
     </router-link>
     <span class="vd">Tìm kiếm</span>
     <input type="text" class="custom-input" placeholder="Tìm kiếm danh mục" />
@@ -15,77 +15,68 @@
     </select>
   </div>
 
-  <!-- Table Desktop -->
-  <div class="table-responsive d-none d-lg-block">
-    <table class="table table-bordered rounded">
-      <thead class="table-light">
-        <tr>
-          <th><input type="checkbox" /></th>
-          <th>STT</th>
-          <th class="d-none d-md-table-cell">Tên</th>
-          <th>Danh mục cha</th>
-          <th>Tuỳ chọn</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><input type="checkbox" /></td>
-          <td>1</td>
-          <td>Chưa phân loại</td>
-          <td>Danh mục mặc định</td>
-          <td class="d-flex justify-content-center gap-2 flex-wrap">
-            <button class="btn btn-outline">Sửa</button>
-            <button class="btn btn-warning">Thêm sản phẩm</button>
-          </td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" /></td>
-          <td>2</td>
-          <td>Mỳ</td>
-          <td></td>
-          <td class="d-flex justify-content-center gap-2 flex-wrap">
-            <button class="btn btn-outline">Sửa</button>
-            <button class="btn btn-danger-delete">Xoá</button>
-            <button class="btn btn-warning">Thêm sản phẩm</button>
-          </td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" /></td>
-          <td>3</td>
-          <td>Mỳ trộn</td>
-          <td>Mỳ</td>
-          <td class="d-flex justify-content-center gap-2 flex-wrap">
-            <button class="btn btn-outline">Sửa</button>
-            <button class="btn btn-danger-delete">Xoá</button>
-            <button class="btn btn-warning">Thêm sản phẩm</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <button class="btn btn-danger-delete delete_desktop">Xoá</button>
-
-  <!-- Mobile View -->
-  <div class="d-block d-lg-none">
-    <div class="card mb-3" v-for="(item, index) in [1,2,3]" :key="index">
-      <div class="row g-0 align-items-center">
-        <div class="col-3 fs-4 fw-bold ps-4">
-          <input type="checkbox" />
-          {{ index + 1 }}
-        </div>
-        <div class="col-9">
-          <div class="card-body">
-            <h5 class="card-title">Chưa phân loại</h5>
-            <p class="card-text"><span class="label">Danh mục cha:</span> </p>
-            <button class="btn btn-outline btn-sm">Sửa</button>
-            <button class="btn btn-danger-delete btn-sm">Xoá</button>
-            <button class="btn btn-warning btn-sm">Thêm sản phẩm</button>
-          </div>
-        </div>
-      </div>
+    <!-- Table Responsive -->
+    <div class="table-responsive d-none d-lg-block">
+        <table class="table table-bordered">
+            <thead class="table-light">
+                <tr>
+                    <th><input type="checkbox" /></th>
+                    <th>Món ăn</th>
+                    <th>Danh mục</th>
+                    <th>Giá thành</th>
+                    <th>Số lượng</th>
+                    <th>Tuỳ chọn</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><input type="checkbox" /></td>
+                    <td>
+                        <img src="/img/food/mykimchihaisan.webp" alt="Mỳ kim chi hải sản" class="me-2 img_thumbnail" />
+                        Mỳ kim chi hải sản
+                    </td>
+                    <td>Mỳ cay</td>
+                    <td>55,000 VNĐ</td>
+                    <td>10</td>
+                    <td class="d-flex gap-2">
+                        <button type="button" class="btn-outline btn-primary">Sửa</button>
+                        <button class="btn-outline btn-danger-delete">Xoá</button>
+                        <button class="btn-outline btn-warning">Ẩn</button>
+                        <button class="btn-outline btn-dark" data-bs-toggle="modal"
+                            data-bs-target="#toppingModal">Toppings</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-  </div>
-  <button class="btn btn-danger-delete delete_mobile">Xoá</button>
+    <button class="btn-outline btn-danger-delete delete_desktop">Xoá</button>
+
+    <!-- Mobile View -->
+    <div class="d-block d-lg-none">
+        <div class="card mb-3">
+            <div class="row g-0 align-items-center">
+                <div class="col-3 d-flex p-1">
+                    <input type="checkbox" name="" id="">
+                    <img src="/img/food/mykimchihaisan.webp" alt="Mỳ kim chi hải sản" class="img-fluid rounded" />
+                </div>
+                <div class="col-9">
+                    <div class="card-body">
+                        <h5 class="card-title">Mỳ kim chi hải sản</h5>
+                        <p class="card-text"><strong>Danh mục:</strong> Mỳ cay</p>
+                        <p class="card-text"><strong>Giá:</strong> 55,000 VNĐ</p>
+                        <p class="card-text"><strong>Số lượng:</strong> 10</p>
+                        <button class="btn-outline btn-primary btn-sm">Sửa</button>
+                        <button class="btn-outline btn-danger-delete btn-sm">Xoá</button>
+                        <button class="btn-outline btn-warning btn-sm">Ẩn</button><button class="btn btn-dark btn-sm"
+                            data-bs-toggle="modal" data-bs-target="#toppingModal">Toppings</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="btn-outline btn-danger-delete delete_mobile">Xoá</button>
+
+
 </template>
 
 <script>
@@ -205,4 +196,26 @@ export default {
     height: 32px;
   }
 }
+.img_thumbnail {
+    width: 50px;
+}
+
+@media (max-width: 768px) {
+  .table-responsive {
+    display: none;
+  }
+
+  .vd {
+    display: none;
+  }
+
+  .delete_desktop {
+    display: none;
+  }
+  .delete_mobile{
+    display: block;
+  }
+
+}
+
 </style>
