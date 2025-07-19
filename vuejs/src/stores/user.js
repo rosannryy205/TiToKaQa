4495d1c864d6696afdd23b3a1d4712913e43e44c
MTuans <tuandmps40831@gmail.com>
2025-07-20 04:23:41 +0700

    Xoá controller cũ: AdminCategoryToppingController và cập nhật các file khác

--- vuejs/src/stores/user.js
+++ vuejs/src/stores/user.js
@@ -1,6 +1,7 @@
 import { ref, onMounted, computed } from 'vue'
 import axios from 'axios'
 import { toast } from "vue3-toastify";
+import Swal from 'sweetalert2'
 
 export const User = {
   setup() {
@@ -13,7 +14,7 @@ export const User = {
       avatar: null,
       username: ''
     })
-    const loading = ref(true); // Controls loading spinner/state visibility
+    const loading = ref(true);
     const isLoggedIn = ref(false);
 
     const userLocal = JSON.parse(localStorage.getItem('user'))
@@ -35,11 +36,11 @@ export const User = {
           avatar: res.data.avatar
             ? (res.data.avatar.startsWith('http') ? res.data.avatar : `http://127.0.0.1:8000/storage/${res.data.avatar}`)
             : null,
-            rank_points: res.data.rank_points,
-            usable_points: res.data.usable_points,
-            rank: res.data.rank,
+          rank_points: res.data.rank_points,
+          usable_points: res.data.usable_points,
+          rank: res.data.rank,
         },
-        tempAvatar.value = null;
+          tempAvatar.value = null;
       } catch (error) {
         console.error('Không lấy được thông tin người dùng', error)
       }
@@ -52,65 +53,140 @@ export const User = {
       }
       return form.value.avatar;
     });
-    const handleImageUpload = (event) => {
+    const handleImageUpload = async (event) => {
       const file = event.target.files[0];
-      if (file) {
-        form.value.avatar = file;
-        tempAvatar.value = URL.createObjectURL(file);
-      } else {
+
+      if (!file) {
         form.value.avatar = null;
         tempAvatar.value = null;
+        return;
       }
-    };
 
+      form.value.avatar = file;
+      tempAvatar.value = URL.createObjectURL(file);
+
+      const formData = new FormData();
+      formData.append("avatar", form.value.avatar);
+
+      try {
+        const response = await axios.post(`http://127.0.0.1:8000/api/user/${user.value.id}/upload-avatar`, formData, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+            // 'Content-Type': 'multipart/form-data'
+          }
+        });
+        form.value.avatar = response.data.avatar_url;
+        tempAvatar.value = null;
+
+        Swal.fire({
+          toast: true,
+          position: 'top-end',
+          icon: 'success',
+          title: 'Ảnh đại diện đã được cập nhật thành công!',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+        });
+        personally(user.value.id);
+
+      } catch (error) {
+        console.error('Lỗi khi cập nhật ảnh đại diện:', error);
+        Swal.fire({
+          toast: false,
+          position: 'top-end',
+          icon: 'error',
+          title: 'Lỗi khi cập nhật ảnh đại diện!',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+        });
+      };
+    }
 
     //  Đăng xuất
     const handleLogout = async () => {
-      try {
-        await axios.post('http://127.0.0.1:8000/api/logout', null, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`
-          }
-        });
+      const result = await Swal.fire({
+        title: 'Bạn có muốn đăng xuất?',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'Xác nhận',
+        cancelButtonText: 'Hủy',
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+      });
+      if (result.isConfirmed) {
+        try {
+          await axios.post('http://127.0.0.1:8000/api/logout', null, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+          });
 
 
-        localStorage.removeItem('user');
-        localStorage.removeItem('token');
-        user.value = null;
-        isLoggedIn.value = false;
-
-        alert('Đăng xuất thành công!');
-        window.location.href = '/';
-      } catch (error) {
-        console.error('Lỗi đăng xuất:', error);
-        alert('Có lỗi xảy ra khi đăng xuất. Vui lòng thử lại!');
+          localStorage.removeItem('user');
+          localStorage.removeItem('token');
+          user.value = null;
+          isLoggedIn.value = false;
+          Swal.fire({
+            toast: true,
+            position: 'top-end',
+            icon: 'success',
+            title: 'Đăng xuất thành công!',
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true
+          })
+          window.location.href = '/';
+        } catch (error) {
+          console.error('Lỗi đăng xuất:', error);
+          Swal.fire({
+            toast: true,
+            position: 'top-end',
+            icon: 'error',
+            title: 'Có lỗi xảy ra khi đăng xuất. Vui lòng thử lại!',
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true
+          })
+        }
       }
     };
 
 
     const handleSubmit = async () => {
       try {
-        const formData = new FormData();
-        formData.append("fullname", form.value.fullname || "");
-        formData.append("phone", form.value.phone || "");
-        formData.append("address", form.value.address || "");
-        if (form.value.avatar instanceof File) {
-          formData.append("avatar", form.value.avatar);
+        const updateProfile = {
+          "fullname": form.value.fullname || "",
+          "phone": form.value.phone || "",
+          "address": form.value.address || "",
         }
-        formData.append("_method", "PATCH");
-
-        await axios.post(`http://127.0.0.1:8000/api/user/${user.value.id}`, formData, {
+        await axios.patch(`http://127.0.0.1:8000/api/user/updateProfile/${user.value.id}`, updateProfile, {
           headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'multipart/form-data',
           }
         });
-        toast.success('Cập nhật thành công!')
-        console.log(form.value)
-        await personally(user.value.id)
+        Swal.fire({
+          toast: true,
+          position: 'top-end',
+          icon: 'success',
+          title: 'Cập nhật thông tin thành công!',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+        })
+        console.log(updateProfile)
+        personally(user.value.id)
       } catch (error) {
         console.error(error)
-        toast.error('Cập nhật thất bại!')
+        Swal.fire({
+          toast: false,
+          position: 'top-end',
+          icon: 'error',
+          title: 'Cập nhật thông tin thất bại!',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+        })
       }
     };
 
