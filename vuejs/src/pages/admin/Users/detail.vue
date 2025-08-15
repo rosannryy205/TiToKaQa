fa002c9f23a2b0eb4167f4a90276523067326e49
thuytien <tiennttps39163@gmail.com>
2025-08-15 16:14:32 +0700

    edit Discount.index, Luckyprize.index

--- vuejs/src/pages/admin/Users/detail.vue
+++ vuejs/src/pages/admin/Users/detail.vue
@@ -113,7 +113,7 @@
                     </td>
                     <td v-else data-label="Sửa"></td>
 
-                    <td v-if="moduleKey == 'food' || moduleKey == 'combo'" data-label="Xoá">
+                    <td v-if="moduleKey == 'food' || moduleKey == 'combo' || moduleKey == 'discounts' || moduleKey == 'luckyprizes'" data-label="Xoá">
                       <div class="checkbox-container">
                         <input class="checkbox-input" type="checkbox" :id="`hidden-${moduleKey}-${currentRoleId}`"
                           v-model="currentRoleAbilities[moduleKey].hidden" @change="handleAbilityChange(moduleKey)"
